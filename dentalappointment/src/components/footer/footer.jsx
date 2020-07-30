f0b11fe9f2292e758e44cbead3790b04c1ade79f
Christian <chsanleo@gmail.com>
2020-07-31 00:26:22 +0200

    Create footer

---
+++ dentalappointment/src/components/footer/footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <h4>Contact us</h4>
+                    <p>111-111-1111</p>
+                    <p>email</p>
+                </div>
+                <div>
+                    <h4>For customers</h4>
+                    <p>About us</p>
+                    <p >Contact us</p>
+                    <p >Privacy policy</p>
+                </div>
+            </div>
+        )
+    }
+}
+export default Footer;
