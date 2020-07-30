3c1bbd9c3faf97d1695147b71a0004b1e86e2c1d
Christian <chsanleo@gmail.com>
2020-07-31 00:11:40 +0200

    Add router

--- dentalappointment/src/App.js
+++ dentalappointment/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import Calendar from './components/calendar.jsx';
 
+import Login from './views/login/login.jsx';
+
+
 function App() {
-  return (
-      <Calendar/>
-   );
+  return(
+    <div>
+      <Router> 
+          <Switch>
+              <Route path="/login" exact component={Login} />
+          </Switch>
+      </Router>
+    </div>
+);
   }
 
 export default App;
