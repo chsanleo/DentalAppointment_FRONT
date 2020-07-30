27f7a69b402fe45f2ddd94b6a9a980083f78a989
Christian <chsanleo@gmail.com>
2020-07-31 00:26:59 +0200

    Show footer and header and calendar

--- dentalappointment/src/App.js
+++ dentalappointment/src/App.js
@@ -3,21 +3,23 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Calendar from './components/calendar.jsx';
-
 import Login from './views/login/login.jsx';
+import Header from './components/header/header.jsx';
+import Footer from './components/footer/footer.jsx';
+import Calendar from './components/calendar/calendar.jsx';
 
 
 function App() {
-  return(
-    <div>
-      <Router> 
-          <Switch>
-              <Route path="/login" exact component={Login} />
-          </Switch>
-      </Router>
-    </div>
-);
-  }
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/login" exact component={Login} />
+        <Route path="/" exact component={Calendar} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
 
 export default App;
