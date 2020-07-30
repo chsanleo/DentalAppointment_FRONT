af5305587a6e2b127c3e3ae4f0954afc7dac7fb4
Christian <chsanleo@gmail.com>
2020-07-31 00:10:38 +0200

    Create userRepository with login method

---
+++ dentalappointment/src/repositories/userRepository.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+
+export const userRepository = {
+    login
+};
+
+async function login(credentials) {
+    try {
+        const res = await axios.post(`main/login`, credentials);
+        console.log(res)
+        return res.data;
+    }
+    catch (error) {
+        throw Error("Could not log in.");
+    }
+};
