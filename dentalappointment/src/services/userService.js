f2b5ccf3c32965300f20ca08654cf8236085b762
Christian <chsanleo@gmail.com>
2020-08-01 20:28:21 +0200

    Create log Out method

--- dentalappointment/src/services/userService.js
+++ dentalappointment/src/services/userService.js
@@ -1,13 +1,19 @@
 import { userRepository } from '../repositories/userRepository.js';
+import { loginAction, logoutAction } from '../redux/actions/user.js';
 
 export const userService = {
-    login
+    login, logout
 };
 
 function login(credentials) {
     userRepository.login(credentials)
         .then(res => {
             localStorage.setItem('authToken', res.token);
+            loginAction(res);
         })
         .catch(error => console.log(error))
+};
+function logout() {
+    localStorage.clear();
+    logoutAction();
 };
