6b3e8c0e6f807c898c9cae21a32ac5f31c7a2e58
Christian <chsanleo@gmail.com>
2020-08-01 20:29:08 +0200

    Add redux to the app

--- dentalappointment/src/index.js
+++ dentalappointment/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import './config/axios.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
