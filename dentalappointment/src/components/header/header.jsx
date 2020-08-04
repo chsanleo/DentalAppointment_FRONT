6f39ff85ab111f8bd6f022f20dd8db47206ff67f
Christian <chsanleo@gmail.com>
2020-08-04 02:04:45 +0200

    import logo to header

--- dentalappointment/src/components/header/header.jsx
+++ dentalappointment/src/components/header/header.jsx
@@ -17,18 +17,17 @@ class Header extends React.Component {
             <header>
                 <div>
                     <NavLink exact to="/">
-                        <img src="" alt="logo" />
+                        <img src="./logoDental.png" className="logo" alt="logo" />
                     </NavLink>
                 </div>
 
-                <div>
-                    <ul>
-                        {this.props.user?.id
-                            ?
-                            <li>
-                                <Link to="/profile">Profile</Link>
-                                <Link to="/logOut">Logout</Link>
-                            </li>
+                <ul>
+                    {this.props.user?.id
+                        ?
+                        <li>
+                            <Link to="/profile">Profile</Link>
+                            <Link to="/logOut">Logout</Link>
+                        </li>
                         :
                         <li>
                             <Link to="/login">Login</Link>
