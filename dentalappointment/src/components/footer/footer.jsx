4ad54db10075c472ba293770534fe6ad7cc649dd
Christian <chsanleo@gmail.com>
2020-08-04 02:05:14 +0200

    import scss
    beauty changes

--- dentalappointment/src/components/footer/footer.jsx
+++ dentalappointment/src/components/footer/footer.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './footer.scss';
 
 class Footer extends React.Component {
     render() {
         return (
-            <div>
-                <div>
+            <footer>
+                <div className="footer-contact">
                     <h4>Contact us</h4>
                     <p>111-111-1111</p>
                     <p>email</p>
                 </div>
-                <div>
+                <div className="footer-links">
                     <h4>For customers</h4>
-                    <p>About us</p>
-                    <p >Contact us</p>
-                    <p >Privacy policy</p>
+                    <Link to="/about">About us</Link>
+                    <p>Privacy policy</p>
                 </div>
-            </div>
+            </footer>
         )
     }
 }
