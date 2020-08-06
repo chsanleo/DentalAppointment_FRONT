f50c771bf0fbe3f997a0e35924414b913190b0a8
Christian <chsanleo@gmail.com>
2020-08-07 00:35:48 +0200

    Complete userRepository methods

--- dentalappointment/src/repositories/userRepository.js
+++ dentalappointment/src/repositories/userRepository.js
@@ -1,12 +1,53 @@
 import axios from 'axios';
 
 export const userRepository = {
-    login
+    login, getProfile, updateUser,
+    deleteUser, signup
 };
 
 async function login(credentials) {
     try {
         const res = await axios.post(`api/auth/login`, credentials);
+        console.log(res)
+        return res.data;
+    }
+    catch (error) {
+        throw Error("Could not log in.");
+    }
+};
+async function signup(email) {
+    try {
+        const res = await axios.post(`api/auth/signup`, email);
+        console.log(res)
+        return res.data;
+    }
+    catch (error) {
+        throw Error("Could not log in.");
+    }
+};
+async function getProfile(userId) {
+    try {
+        const res = await axios.get(`api/user/` + userId);
+        console.log(res)
+        return res.data;
+    }
+    catch (error) {
+        throw Error("Could not log in.");
+    }
+};
+async function updateUser(user) {
+    try {
+        const res = await axios.put(`api/user/` + user.userId, user);
+        console.log(res)
+        return res.data;
+    }
+    catch (error) {
+        throw Error("Could not log in.");
+    }
+};
+async function deleteUser(userId) {
+    try {
+        const res = await axios.delete(`api/user/` + userId);
         console.log(res)
         return res.data;
     }
