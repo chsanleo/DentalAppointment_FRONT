2667baefb52d677c765a412aec672de2237d4d68
Christian <chsanleo@gmail.com>
2020-08-06 03:33:04 +0200

    Create contactMail route, change landing for login

--- dentalappointment/src/App.js
+++ dentalappointment/src/App.js
@@ -10,17 +10,19 @@ import Calendar from './components/calendar/calendar.jsx';
 import Page404 from './views/404/404.jsx';
 import AboutUs from './views/about/about.jsx'
 import AskExp from './views/askExp/askExp.jsx';
+import ContactMail from './views/contactUs/contactUs.jsx';
 
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
-        <Route path="/login" exact component={Login} />
-        <Route path="/" exact component={Calendar} />
-        <Route path="/about" exact component={AboutUs}/>
-        <Route path="/access" exact component={AskExp}/>
-        
+        <Route path='/' exact component={Login} />
+        <Route path='/agenda' exact component={Calendar} />
+        <Route path='/about' exact component={AboutUs}/>
+        <Route path='/access' exact component={AskExp}/>
+        <Route path='/contactMail' exact component={ContactMail}/>
+
           <Route component={Page404} />
       </Switch>
         <Footer />
