4ef60d56a2ee89dc4ec2030c633f4a0e711b7a1e
Christian <chsanleo@gmail.com>
2020-08-10 21:48:39 +0200

    add appointment to reducer index

--- dentalappointment/src/redux/reducer/index.js
+++ dentalappointment/src/redux/reducer/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 
 import usersReducer from './user.js';
+import appointmentReducer from './appointment.js';
 
 const reducer = combineReducers({
     users: usersReducer,
-
+    appointments : appointmentReducer
 });
 export default reducer;
