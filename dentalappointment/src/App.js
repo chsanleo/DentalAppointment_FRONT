0d72cb1681844d03298dd74de929b537ba68df7b
Christian <chsanleo@gmail.com>
2020-08-10 21:45:36 +0200

    delete old calendar and add new agenda

--- dentalappointment/src/App.js
+++ dentalappointment/src/App.js
@@ -6,7 +6,6 @@ import './App.scss';
 import Login from './views/login/login.jsx';
 import Header from './components/header/header.jsx';
 import Footer from './components/footer/footer.jsx';
-import Calendar from './components/calendar/calendar.jsx';
 import Page404 from './views/404/404.jsx';
 import AboutUs from './views/about/about.jsx'
 import AskExp from './views/askExp/askExp.jsx';
@@ -14,6 +13,7 @@ import ContactMail from './views/contactUs/contactUs.jsx';
 import ForgotPass from './views/forgotPass/forgotPass.jsx';
 import Profile from './views/profile/profile.jsx';
 import Logout from './views/logout/logout.jsx';
+import Agenda from './components/agenda/agenda.jsx';
 
 function App() {
   return (
@@ -21,13 +21,13 @@ function App() {
       <Header />
       <Switch>
         <Route path='/' exact component={Login} />
-        <Route path='/agenda' exact component={Calendar} />
         <Route path='/about' exact component={AboutUs} />
         <Route path='/access' exact component={AskExp} />
         <Route path='/contactMail' exact component={ContactMail} />
         <Route path='/forgotPass' exact component={ForgotPass} />
         <Route path='/profile' exact component={Profile} />
         <Route path='/logout' exact component={Logout} />
+        <Route path='/agenda' exact component={Agenda} />
 
         <Route component={Page404} />
       </Switch>
